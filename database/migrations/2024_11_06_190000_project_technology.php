a4a42297a05816718b32eee7184285ea4224eadc
ChristopherS24 <chryskies@gmail.com>
2024-11-07 00:46:19 +0100

    aggiunta tab pivot e relazione many to many

---
+++ database/migrations/2024_11_06_190000_project_technology.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        schema::create('project_technology', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('projects')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->unsignedBigInteger('technology_id');
+            $table->foreign('technology_id')
+                ->references('id')
+                ->on('technologies')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_technologies');
+    }
+};
